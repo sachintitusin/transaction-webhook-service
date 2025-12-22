2c60d2fc1c461a88334d864267f7793c2a306c21
Sachin Titus <sachinottawa@gmail.com>
2025-12-22 11:48:38 +0530

    feat: add transaction request and response schemas

---
+++ app/schemas/transaction.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import datetime
+from app.models.transaction import TransactionStatus
+
+class TransactionCreate(BaseModel):
+    transaction_id: str = Field(..., min_length=1)
+    source_account: str = Field(..., min_length=1)
+    destination_account: str = Field(..., min_length=1)
+    amount: int = Field(..., gt=0)
+    currency: str = Field(..., min_length=1)
+
+class TransactionResponse(BaseModel):
+    transaction_id: str
+    source_account: str
+    destination_account: str
+    amount: int
+    currency: str
+    status: TransactionStatus
+    created_at: datetime
+    processed_at: Optional[datetime]
+
+    # Config is an inner configuration class for Pydantic
+    class Config:
+        # Returning correct JSON instead of SQLAlchemy obj
+        # We do it only on responses
+        orm_mode = True
